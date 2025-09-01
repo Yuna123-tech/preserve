227e61e2e5befc0c23c33afc72060d347a19852b
Yuna123-tech <dbsdk0405@gmail.com>
2025-09-02 08:55:10 +0900

    Delete components directory

--- components/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Card: React.FC<{ children: React.ReactNode; className?: string; }> = ({ children, className = '' }) => (
-  <div className={`bg-white rounded-2xl shadow-lg p-6 transition-all duration-300 hover:shadow-xl hover:scale-[1.03] ${className}`}>
-    {children}
-  </div>
-);
-
-export default Card;
