2d35a4c99040e152f177a27541bb7f481bd4d512
Yuna123-tech <dbsdk0405@gmail.com>
2025-09-02 09:04:55 +0900

    Delete types.ts

--- types.ts
+++
@@ -1,8 +0,0 @@
-export type Page = 'home' | 'learn' | 'explore' | 'quiz' | 'videos' | 'protect-heritage-game';
-
-export interface QuizQuestion {
-  question: string;
-  options: string[];
-  correctAnswerIndex: number;
-  explanation: string;
-}
